b66c1d6fc629f0cf0ae8331d7c0f1ae65cdd58f7
Diary13 <dr.andriamampitasoa@gmail.com>
2023-11-11 11:48:11 +0300

    create card weather

--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -1,9 +1,15 @@
+import '../styles/cardWeather.scss';
 
 const CardWeather = () => {
     return (
-        <>
-            <div>card</div>
-        </>
+        <div className="card">
+            <h2 className='temperature'>20°C</h2>
+            <div className="weather">
+                <h2 className='typeWeather'>Storm rain</h2>
+                <img src="./src/assets/images/storm-rain.png" alt="" />
+            </div>
+            <h3 className='date'>Tue 28, November 23</h3>
+        </div>
     )
 }
 
